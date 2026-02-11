5f0cd7bfd658e1cb33ec71ef1bbaf4a92e0ff2d5
rafivalih <rafivalihshaik@gmail.com>
2026-02-11 15:29:23 +0530

    Updated template image paths

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter , Routes, Route } from "react-router-dom";
 import { ResumeProvider } from "@/context/ResumeContext";
 import Navbar from "@/components/Navbar";
 import Home from "./pages/Home";
@@ -20,7 +20,7 @@ const App = () => (
       <ResumeProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <HashRouter >
           <div className="min-h-screen flex flex-col">
             <Navbar />
             <main className="flex-1">
@@ -34,7 +34,7 @@ const App = () => (
               </Routes>
             </main>
           </div>
-        </BrowserRouter>
+        </HashRouter >
       </ResumeProvider>
     </TooltipProvider>
   </QueryClientProvider>
